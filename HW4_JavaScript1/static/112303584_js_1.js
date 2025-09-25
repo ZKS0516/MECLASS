205463f434aea10f925c3d09589053e37d7fc335
ZKS <f130410529@gmail.com>
2025-09-25 14:35:57 +0800

    feat:對js1新增console.log(answer)方便debug debug:修改if else順序，避免狀況同時發生 feat:對html1新增輸入框和"猜"按鈕

--- HW4_JavaScript1/static/112303584_js_1.js
+++ HW4_JavaScript1/static/112303584_js_1.js
@@ -1,24 +1,30 @@
 
 let answer = Math.floor(Math.random() * 100) + 1;
 let attempts = 0;
+console.log(answer);
 
 function checkGuess(){
     const guess = Number(document.getElementById("guess").value);
     attempts++;
-    if (guess === answer) {
-        alert("恭喜你猜對了，你總共猜了"+attempts+"次。");
+    if (guess > 100 || guess < 0) {
+        alert("超出範圍，請重新輸入");
         return;
       }
 
-    if (guess > answer && guess <= 100) {
+    else if (guess > answer && guess <= 100) {
         alert("太大了，再猜猜看");
+        return;
     } 
     
     else if (guess < answer && guess >= 0) {
         alert("太小了，再猜猜看");
+        return;
     } 
-
+    
     else {
-        alert("輸入錯誤，請重新輸入");
+        alert("恭喜你猜對了，你總共猜了"+attempts+"次。");
+        answer = Math.floor(Math.random() * 100) + 1;
+        attempts = 0;
+        console.log(answer);
     }
 }
