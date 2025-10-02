ca10eed92201529a544b3503cc1086f8a866a96b
ZKS <f130410529@gmail.com>
2025-10-02 14:02:28 +0800

    feat:修改檔名，把alert提示改成網頁文字(innerHTML)

--- HW5 JavaScript2/static/112303584_js_1.js
+++ HW5 JavaScript2/static/112303584_js_1.js
@@ -3,6 +3,8 @@ let answer = Math.floor(Math.random() * 101); //隨機產生0-100(包刮0和100)
 let attempts = 0; //初始化嘗試次數=0
 console.log(answer); //先在主控台印出答案，方便debug
 
+const hint = document.getElementById("hint");
+
 function checkGuess(){
     const guess = Number(document.getElementById("guess").value);
     //宣告常數guess(使用者的輸入不能被更改)，透過getElementById找id="guesss"，並把這個"物件"的value先Number再存入guess
@@ -10,15 +12,15 @@ function checkGuess(){
     attempts++; //嘗試次數+1
 
     if (guess > 100 || guess < 0) {
-        alert("超出範圍，請重新輸入"); //訂個上下限，避免使用者亂來
+        hint.innerHTML="hint:超出範圍，請重新輸入"; //訂個上下限，避免使用者亂來
       }
 
     else if (guess > answer && guess <= 100) {
-        alert("太大了，再猜猜看");
+        hint.innerHTML="hint:太大了，再猜猜看";
     } 
     
     else if (guess < answer && guess >= 0) {
-        alert("太小了，再猜猜看");
+        hint.innerHTML="hint:太小了，再猜猜看";
     } 
     
     else {
